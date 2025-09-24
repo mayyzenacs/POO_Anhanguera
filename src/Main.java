a3bde9d174d39190aaca73d2b2c94d2c681f65b4
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-09-23 23:48:00 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,24 @@
-abstract class Pessoa {
-    protected String nome;
-    protected int idade;
-    protected String endereco;
-
-
-    public Pessoa(String nome, int idade, String endereco) {
-        this.nome = nome;
-    }
-    public abstract void CalcularSalario();
+abstract class operacaoMatematica {
+    public abstract double calcular(double a, double b) throws Exception;
 }
 
-class Aluno extends Pessoa {
-    private int Matricula;
-    public Aluno(String nome, int idade, String endereco, int Matricula) {
-        super(nome, idade, endereco);
-        this.Matricula = Matricula;
-    }
-
+class Soma extends operacaoMatematica {
     @Override
-    public void CalcularSalario() {
-        int[] arr = {300,600,1200,4500,600};
-        try {
-            // System.out.println(nome + " Calculando salario");
-            System.out.println(arr[10]);
-            throw new ArrayIndexOutOfBoundsException("fora do index");
+    public double calcular(double a, double b) {
+        return a + b;
+    }
 
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getMessage());
-        }
-        finally {
-            System.out.println(nome + " Finalizando salario");
+}
+class Divisao extends operacaoMatematica {
+    @Override
+    public double calcular(double a, double b) {
+        if (b == 0) {
+
         }
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
-        Aluno aluno1 = new Aluno("Lucas", 24, "Limoeiro", 4523);
-        aluno1.CalcularSalario();
+class DivisaoPorZeroException extends Exception {
+    public divisaoPorZeroExcepetion() {
+        super("Não é possivel dividir por 0")
     }
 }
