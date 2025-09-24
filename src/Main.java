b233b02ea4eeaae982165d51e8fd654ebc2d12c9
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-09-24 18:29:59 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,38 @@
-abstract class operacaoMatematica {
+abstract class OperacaoMatematica {
     public abstract double calcular(double a, double b) throws Exception;
 }
 
-class Soma extends operacaoMatematica {
+class Soma extends OperacaoMatematica {
     @Override
     public double calcular(double a, double b) {
         return a + b;
     }
 
 }
-class Divisao extends operacaoMatematica {
+class Divisao extends OperacaoMatematica {
     @Override
-    public double calcular(double a, double b) {
+    public double calcular(double a, double b) throws DivisaoPorZeroException {
         if (b == 0) {
-
+            throw new DivisaoPorZeroException();
         }
+        return a / b;
     }
 }
 class DivisaoPorZeroException extends Exception {
-    public divisaoPorZeroExcepetion() {
-        super("Não é possivel dividir por 0")
+    public DivisaoPorZeroException() {
+        super("Não é possível dividir por zero.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        OperacaoMatematica soma = new Soma();
+        OperacaoMatematica divisao = new Divisao();
+        try {
+            System.out.println("Soma resultado: " + soma.calcular(15,20));
+            System.out.println("Divisão resultado: " + divisao.calcular(15,0));
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 }
